ddd1214c33cc5134863c04293291f5fb92e83ad7
Kevin Reyes <kevin.reyes1990@hotmail.com>
2022-05-26 14:52:31 +1000

    Added if/else flag to check whether page has been accessed restrictedly

--- logoff.php
+++ logoff.php
@@ -5,8 +5,11 @@
    // get access to the session variables
    session_start();
 
-   // Destroy the session
-   session_destroy();
+   // If the retricted access variable has been flagged, do not destroy the session variable
+   if (!isset($_SESSION["error"])){
+        session_destroy();
+   }
+
 
    // Redirect the user to the starting page (login.php)
    header("location: index1.php");
